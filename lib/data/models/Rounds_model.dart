3a28985f3fdb4df61f31c1677dddc962b8c43529
Israa Ezzat <62281743+israa639@users.noreply.github.com>
2022-05-17 17:48:18 -0700

    poses_screen

---
+++ lib/data/models/Rounds_model.dart
@@ -0,0 +1,7 @@
+import 'package:yoga_ai/data/models/pose.dart';
+class Rounds{
+  final String round_name;
+  final List<Pose> round_poses;
+
+  Rounds({required this.round_name,required this.round_poses});
+}
